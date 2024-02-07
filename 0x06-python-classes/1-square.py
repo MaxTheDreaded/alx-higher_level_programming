6c7a1695ebaab645118dcbb33d31b54a8a83f566
Jesse Otu <chibuzorotu98@gmail.com>
2024-02-07 23:51:52 +0000

    added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represents the size of the square defined
+        """
+
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -3,3 +3,6 @@
 This folder contains the code for the ALX's Python - Classes and Objects course.
 
 ## Tasks
+
+- [0. My first square](./0-square.py)
+- [1. Square with size](./1-square.py)
