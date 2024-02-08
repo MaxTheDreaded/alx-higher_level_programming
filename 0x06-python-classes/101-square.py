2c826a42405ccef4fffceeaf4b48612789365d1d
Jesse Otu <chibuzorotu98@gmail.com>
2024-02-08 00:46:43 +0000

    added 101-square.py

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+"""Defines a square by: (based on 6-square.py)"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initialize the object with the given size and position.
+
+        Parameters:
+            size (int): The size of the object.
+            position (tuple): The position of the object as a
+              tuple of (x, y) coordinates.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set position"""
+        if (
+            not isinstance(value, tuple)
+            or len(value) != 2
+            or not all(isinstance(i, int) for i in value)
+            or any(i < 0 for i in value)
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
