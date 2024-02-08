6b79e236f168ba7ccdf0a1b4dd9cd32183aac9cb
Jesse Otu <chibuzorotu98@gmail.com>
2024-02-08 01:07:15 +0000

    added 102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Defines a square by: (based on 4-square.py)"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args: size - represents the size of the square defined
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Retrieves size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets size to a value"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
