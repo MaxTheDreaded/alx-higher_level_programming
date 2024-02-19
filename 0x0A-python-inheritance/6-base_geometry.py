8b1b7eb2a6197ffb806193606c35e3fb6cc1dae8
Jesse Otu <chibuzorotu98@gmail.com>
2024-02-19 22:52:53 +0000

    added 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry based on 5-base_geometry."""
+
+
+class BaseGeometry:
+	"""Defines a class BaseGeometry."""
+
+	def area(self):
+		"""Not implemented."""
+		raise Exception("area() is not implemented")
