153e132b684ec93edeaa881ee29330ce9f5dd0e5
Jesse Otu <chibuzorotu98@gmail.com>
2024-02-19 23:31:01 +0000

    added 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Defines a Square class that inherits from Rectangle."""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new Square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the Square."""
+        return self.__size ** 2
