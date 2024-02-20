440e4fadd18b0dad3f733e9ba7feefa8b1bf8e49
Jesse Otu <chibuzorotu98@gmail.com>
2024-02-20 23:18:43 +0000

    added 100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a function that inserts a line of text to a file,\
+after each line containing a specific string."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text to a file, after each line containing a
+    specific string."""
+    with open(filename, 'r', encoding="utf-8") as f:
+        lines = f.readlines()
+    with open(filename, 'w', encoding="utf-8") as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
