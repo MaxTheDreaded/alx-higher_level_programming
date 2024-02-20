bbe7d91529c8031c745cdf0d063a50040584be18
Jesse Otu <chibuzorotu98@gmail.com>
2024-02-20 22:47:41 +0000

    updated 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a function that writes a string to a text file adn returns the number of characters written."""
+"""Defines a function that writes a string to a text file."""
 
 
 def write_file(filename="", text=""):
